1ada9b7f48fb93bc57dff0bd41f6a4b71e164486
saaha1995 <romanko.aleksandr.a@gmail.com>
2023-03-13 15:12:23 +0300

    initial commit

---
+++ config.el
@@ -0,0 +1,121 @@
+;;; $DOOMDIR/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here! Remember, you do not need to run 'doom
+;; sync' after modifying this file!
+
+
+;; Some functionality uses this to identify you, e.g. GPG configuration, email
+;; clients, file templates and snippets. It is optional.
+(setq user-full-name "John Doe"
+      user-mail-address "john@doe.com")
+
+;; Doom exposes five (optional) variables for controlling fonts in Doom:
+;;
+;; - `doom-font' -- the primary font to use
+;; - `doom-variable-pitch-font' -- a non-monospace font (where applicable)
+;; - `doom-big-font' -- used for `doom-big-font-mode'; use this for
+;;   presentations or streaming.
+;; - `doom-unicode-font' -- for unicode glyphs
+;; - `doom-serif-font' -- for the `fixed-pitch-serif' face
+;;
+;; See 'C-h v doom-font' for documentation and more examples of what they
+;; accept. For example:
+;;
+;;(setq doom-font (font-spec :family "Fira Code" :size 12 :weight 'semi-light)
+;;      doom-variable-pitch-font (font-spec :family "Fira Sans" :size 13))
+;;
+;; If you or Emacs can't find your font, use 'M-x describe-font' to look them
+;; up, `M-x eval-region' to execute elisp code, and 'M-x doom/reload-font' to
+;; refresh your font settings. If Emacs still can't find your font, it likely
+;; wasn't installed correctly. Font issues are rarely Doom issues!
+
+;; There are two ways to load a theme. Both assume the theme is installed and
+;; available. You can either set `doom-theme' or manually load a theme with the
+;; `load-theme' function. This is the default:
+
+;;(setq doom-theme 'doom-gruvbox)
+;;(setq doom-theme 'doom-gruvbox)
+;;(setq doom-theme 'doom-gruvbox)
+;;(setq doom-theme 'doom-gruvbox)
+;;(setq doom-theme 'doom-gruvbox)
+;;(setq doom-theme 'doom-gruvbox)
+;;(setq doom-theme 'doom-gruvbox)
+;;(setq doom-theme 'doom-vibrant)
+
+
+;;(setq doom-theme 'doom-one) ;; default works well
+;;(setq doom-theme 'doom-xcode)
+(setq doom-theme 'doom-gruvbox)
+
+;; This determines the style of line numbers in effect. If set to `nil', line
+;; numbers are disabled. For relative line numbers, set this to `relative'.
+(setq display-line-numbers-type `relative)
+
+;; If you use `org' and don't want your org files in the default location below,
+;; chage `org-directory'. It must be set before org loads!
+(setq org-directory "~/org/")
+
+
+;; Whenever you reconfigure a package, make sure to wrap your config in an
+;; `after!' block, otherwise Doom's defaults may override your settings. E.g.
+;;
+;;   (after! PACKAGE
+;;     (setq x y))
+;;
+;; The exceptions to this rule:
+;;
+;;   - Setting file/directory variables (like `org-directory')
+;;   - Setting variables which explicitly tell you to set them before their
+;;     package is loaded (see 'C-h v VARIABLE' to look up their documentation).
+;;   - Setting doom variables (which start with 'doom-' or '+').
+;;
+;; Here are some additional functions/macros that will help you configure Doom.
+;;
+;; - `load!' for loading external *.el files relative to this one
+;; - `use-package!' for configuring packages
+;; - `after!' for running code after a package has loaded
+;; - `add-load-path!' for adding directories to the `load-path', relative to
+;;   this file. Emacs searches the `load-path' when you load packages with
+;;   `require' or `use-package'.
+;; - `map!' for binding new keys
+;;
+;; To get information about any of these functions/macros, move the cursor over
+;; the highlighted symbol at press 'K' (non-evil users must press 'C-c c k').
+;; This will open documentation for it, including demos of how they are used.
+;; Alternatively, use `C-h o' to look up a symbol (functions, variables, faces,
+;; etc).
+;;
+;; You can also try 'gd' (or 'C-c c d') to jump to their definition and see how
+;; they are implemented.
+
+
+;(use-package reverse-im
+;     :custom
+;     (reverse-im-input-methods ("russian-computer"))
+;     :config
+;     (reverse-im-mode t))
+
+(use-package! reverse-im
+  :config
+  (reverse-im-activate "russian-computer"))
+
+(setq doom-font (font-spec :family "Droid Sans Mono" :size 14 :weight 'light)
+      doom-variable-pitch-font (font-spec :family "Noto Serif" :size 16))
+
+
+(add-hook 'prog-mode-hook #'rainbow-delimiters-mode)
+
+
+;; map keys for autocompleat in company plugin
+;; unbind <return> and bind complete to <C-SPC> and <C-return>
+
+(with-eval-after-load 'company
+  (define-key company-active-map (kbd "<return>") nil)
+  (define-key company-active-map (kbd "RET") nil)
+  (define-key company-active-map (kbd "C-SPC") #'company-complete-selection)
+  (define-key company-active-map (kbd "<C-return>") #'company-complete-selection))
+
+;; map key for eval multiple lines in ess in visual mode
+
+(define-key evil-visual-state-map (kbd "C-<return>") #'ess-eval-region-or-line-visibly-and-step)
+
